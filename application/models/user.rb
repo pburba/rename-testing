5a483351f651d873b1d25965a21110db16585963
TeamForge Administrator <root@ctf-dev-build-box.collab.net>
2015-07-13 15:22:29 -0400

    Modifiy a file
    Change-Id: I00f7993b2293b2453e14977ca1d2c1031c287150

--- application/models/user.rb
+++ application/models/user.rb
@@ -21,4 +21,4 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
-end
+end#new line 1
